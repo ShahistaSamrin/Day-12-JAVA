c4bdae46193886d5c32baf64dc5121708f14ae86
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-13 18:56:43 +0530

    Add files via upload

---
+++ Mapinterface.java
@@ -0,0 +1,10 @@
+/*  {SNO=01, SNAME=SHAHI}  */
+import java.util.*;
+public class Mapinterface{
+    public static void main(String args[]){
+        HashMap<String,String> a=new HashMap<>();
+        a.put("SNO","01");
+        a.put("SNAME","SHAHI");
+        System.out.println(a);
+    }
+}
